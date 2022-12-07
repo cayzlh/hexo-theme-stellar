9020777c998e00a3a8021b40fdf6966b709d2336
chenanyu <ant.chenanyu@icloud.com>
2022-12-07 20:27:55 +0800

    1207 - 主题升级

--- scripts/events/lib/config.js
+++ scripts/events/lib/config.js
@@ -22,7 +22,7 @@ module.exports = hexo => {
   }
 
   if (cache && cache.enable && hexo.config.relative_link) {
-    warning('caching', '`relative_link` option in Hexo `_config.yml`');
+    warning('caching', '`relative_link` option in Hexo `_config.yml.bak`');
     hexo.config.relative_link = false;
   }
   // hexo.config.meta_generator = false;
