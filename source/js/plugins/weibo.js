957032bf1857ebbc165d34280143134bb733d743
chenanyu <ant.chenanyu@icloud.com>
2022-12-25 17:01:40 +0800

    1225 - 升级主题到 1.18.5

--- source/js/plugins/weibo.js
+++ source/js/plugins/weibo.js
@@ -78,7 +78,7 @@ const weibojs = {
         cell += '</div>';
         cell += '<a class="item comments last" href="' + item.url + '#issuecomment-new" target="_blank" rel="external nofollow noopener noreferrer">';
         cell += '<span><svg t="1666270368054" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="2528" width="200" height="200"><path d="M952 64H72C32.3 64 0 96.3 0 136v508c0 39.7 32.3 72 72 72h261l128 128c14 14 32.5 21.1 50.9 21.1s36.9-7 50.9-21.1l128-128h261c39.7 0 72-32.3 72-72V136c0.2-39.7-32.1-72-71.8-72zM222 462c-39.8 0-72-32.2-72-72s32.2-72 72-72 72 32.2 72 72-32.2 72-72 72z m290-7.7c-39.8 0-72-32.2-72-72s32.2-72 72-72 72 32.2 72 72c0 39.7-32.2 72-72 72z m290 8c-39.8 0-72-32.2-72-72s32.2-72 72-72 72 32.2 72 72c0 39.7-32.2 72-72 72z" p-id="2529"></path></svg> '
-         + (item.comments_count || 0) + '</span>';
+            + (item.comments_count || 0) + '</span>';
         cell += '</a>';
         cell += '<div class="item reaction attitudes">';
         cell += '<a class="item comments last" href="' + item.url + '#issuecomment-new" target="_blank" rel="external nofollow noopener noreferrer">';
@@ -110,7 +110,7 @@ $(function () {
     var cfg = new Object();
     cfg.el = el;
     cfg.api = api;
-    cfg.avatar = 'https://fastly.jsdelivr.net/gh/cdn-x/placeholder@1.0.1/avatar/round/3442075.svg';
+    cfg.avatar = 'https://gcore.jsdelivr.net/gh/cdn-x/placeholder@1.0.4/avatar/round/3442075.svg';
     weibojs.layoutDiv(cfg);
   }
 });
--- source/js/search/local-search.js
+++ source/js/search/local-search.js
@@ -28,9 +28,9 @@ var searchFunc = function(path, filter, searchId, contentId) {
     var i, j, result = [];
 
     for (i = 0; i < keywords.length; i++) {
-        for (j = i + 1; j < keywords.length + 1; j++) {
-            result.push(keywords.slice(i, j).join(" "));
-        }
+      for (j = i + 1; j < keywords.length + 1; j++) {
+        result.push(keywords.slice(i, j).join(" "));
+      }
     }
     return result;
   }
@@ -47,20 +47,17 @@ var searchFunc = function(path, filter, searchId, contentId) {
       $input.addEventListener("input", function(){
         var resultList = [];
         var keywords = getAllCombinations(this.value.trim().toLowerCase().split(" "))
-          .sort(function(a,b) { return b.split(" ").length - a.split(" ").length; });
+            .sort(function(a,b) { return b.split(" ").length - a.split(" ").length; });
         $resultContent.innerHTML = "";
         if (this.value.trim().length <= 0) {
           return;
         }
         // perform local searching
         datas.forEach(function(data) {
-          if (!data.title?.trim().length) { return }
           if (!data.content?.trim().length) { return }
           var matches = 0;
-          if (filter && !data.path.includes(filter)) {
-            return;
-          }
-          var dataTitle = data.title.trim();
+          if (filter && !data.path.includes(filter)) { return }
+          var dataTitle = data.title?.trim() || 'Untitled';
           var dataTitleLowerCase = dataTitle.toLowerCase();
           var dataContent = data.content;
           var dataContentLowerCase = dataContent.toLowerCase();
@@ -123,7 +120,7 @@ var searchFunc = function(path, filter, searchId, contentId) {
         });
         if (resultList.length) {
           resultList.sort(function(a, b) {
-              return b.rank - a.rank;
+            return b.rank - a.rank;
           });
           var result ="<ul class=\"search-result-list\">";
           for (var i = 0; i < resultList.length; i++) {
